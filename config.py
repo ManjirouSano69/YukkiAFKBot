8503d48e364e1e661e50b2db277dc745cd148c25
ManjirouSano69 <168046394+ManjirouSano69@users.noreply.github.com>
2024-06-09 11:34:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID", "22867431"))
-API_HASH = getenv("API_HASH", "24ef0e76ceb137563dc33722e4cd79bd")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7483130894:AAH9VSFstcInQmPS2NGEKv5ivcq_XtAfkx8")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://niksharma92297:afk@cluster0.wj4ftow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "1993048420 5743248220 5150045729 1214348787 6297086120").split())
+    map(int, getenv("SUDO_USER", "").split())
 )  # Input type must be interger
